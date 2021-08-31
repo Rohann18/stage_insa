f8c13b520330df34ac63f896f39e02f9172604a3
Rohann18 <74454066+Rohann18@users.noreply.github.com>
2021-08-31 18:03:19 +0200

    Update spades3.rs

--- spades3.rs
+++ spades3.rs
@@ -15,11 +15,13 @@ use rand::Rng;
 use std::convert::TryInto;
 use sha2::Sha512;
 
+/* Struct du vecteur c*/
 struct Chiffre {
     x: RistrettoPoint,
     y: RistrettoPoint,
 }
 
+/* struct pour la partie de l'argument de mélange de la preuve de Groth */
 struct Arg {
     c : RistrettoPoint,
     c_d : RistrettoPoint,
@@ -31,6 +33,7 @@ struct Arg {
     z_delta : Scalar,
 }
 
+/* Chaque struct Zk contient tous les éléments que le vérifieur a besoin */
 struct Zkrand {
     c : RistrettoPoint,
     cd : RistrettoPoint,
@@ -55,14 +58,6 @@ struct Zktheta {
     g : RistrettoPoint,
 }
 
-struct State {
-    alpha : u32,
-    suit : u32,
-    u : Vec<Vec<usize>>,
-}
-
-// Valeurs à mettre dans le struct : la carte id, la clé publique pk, le chiffre ch, t1, t2, z, g
-
 struct Zkpi0 {
     id : u32,
     pk : RistrettoPoint,
@@ -72,8 +67,6 @@ struct Zkpi0 {
     z : Scalar,
     g : RistrettoPoint,
 }
-
-// Valeurs à retourner : g1, g2, pk1, pk2, y1, y2, z, c, l
 
 struct Zkpij {
     b : bool,
@@ -87,6 +80,14 @@ struct Zkpij {
     c : Vec<Scalar>,
 }
 
+/* Structure permettant de suivre le bon déroulement du jeu */
+struct State {
+    alpha : u32,
+    suit : u32,
+    u : Vec<Vec<usize>>,
+}
+
+/* Fonction qui crée le deck de cartes */
 fn deck() -> Vec <u32> {
     let mut deck = Vec::new();
     for i in 1..5 {
@@ -98,12 +99,14 @@ fn deck() -> Vec <u32> {
     deck
 }
 
+/* Fonction qui choisit aléatoirement un générateur g*/
 fn init() -> RistrettoPoint {
     let mut rng = OsRng;
     let g = RistrettoPoint::random(&mut rng);
     g
 }
 
+/* Fonction de génération des clés publiques en choississant aléatoirement une clé secrète */
 fn keygen(g : RistrettoPoint) -> (Scalar,RistrettoPoint) {
     let mut rng = OsRng;
     let sk : Scalar = Scalar ::random(&mut rng);
@@ -111,6 +114,7 @@ fn keygen(g : RistrettoPoint) -> (Scalar,RistrettoPoint) {
     (sk,pk)
 }
 
+/* Fonction qui va mélanger le deck de cartes et distribuer les cartes à la fin */
 fn gkeygen(mut ch : Vec <Chiffre> , chiffre : &mut Vec<Chiffre>, sk : &Vec <Scalar> , g : &RistrettoPoint, pkc : &RistrettoPoint) {
     let mut rng = thread_rng();
     let mut rng2 = OsRng;
@@ -141,6 +145,7 @@ fn gkeygen(mut ch : Vec <Chiffre> , chiffre : &mut Vec<Chiffre>, sk : &Vec <Scal
     }
 }
 
+// Fonction qui permet de mélanger le deck avec aussi la preuve de rand
 fn rand(ch : &mut Vec<Chiffre>, nums : &Vec<usize>, r : Vec<Scalar>, zk_r : &mut Vec<Zkrand>, g : &RistrettoPoint, pkc : &RistrettoPoint) {
     let mut ch2 : Vec <Chiffre> = Vec::new();
     for k in 0..52 {
@@ -150,9 +155,9 @@ fn rand(ch : &mut Vec<Chiffre>, nums : &Vec<usize>, r : Vec<Scalar>, zk_r : &mut
     let zkr = zkrand(&ch,&ch2,&nums,&r,&g,&pkc);
     zk_r.push(zkr);
     *ch = ch2;
-    tps
 }
 
+// Fonction qui choisit aléatoirement des générateurs pour les commitment de la preuve rand
 fn setup(n : &usize) -> (Vec<RistrettoPoint>,RistrettoPoint) {
     let mut rng = OsRng;
     let mut g : Vec<RistrettoPoint> = Vec::new();
@@ -164,6 +169,7 @@ fn setup(n : &usize) -> (Vec<RistrettoPoint>,RistrettoPoint) {
     (g,h)
 }
 
+// Fonction qui calcule des vecteurs commitment de Pedersen
 fn com(x : &Vec<Scalar>, r : &Scalar, g : &Vec<RistrettoPoint>, h : &RistrettoPoint) -> RistrettoPoint {
     let mut res = r * h;
     for i in 0..x.len(){
@@ -172,6 +178,7 @@ fn com(x : &Vec<Scalar>, r : &Scalar, g : &Vec<RistrettoPoint>, h : &RistrettoPo
     res
 }
 
+/* Fonction qui effectue la preuve rand jusqu'au moment des calculs du vérifieur */
 fn zkrand(che : &Vec<Chiffre>, chs : &Vec<Chiffre>, nums : &Vec<usize>, ra : &Vec<Scalar>, g : &RistrettoPoint, pkc : &RistrettoPoint) -> Zkrand {
     let n = nums.len();
     let mut rng = OsRng;
@@ -244,6 +251,7 @@ fn zkrand(che : &Vec<Chiffre>, chs : &Vec<Chiffre>, nums : &Vec<usize>, ra : &Ve
     zkr
 }
 
+/* Fonction qui effectue la partie arg preuve rand jusqu'au moment des calculs du vérifieur */
 fn arg(c : &RistrettoPoint, nums : &Vec<usize>, r : &Scalar, m : &Vec<Scalar>, g : &Vec<RistrettoPoint>, h : &RistrettoPoint) -> Arg {
     let n = nums.len();
     let mut rng = OsRng;
@@ -304,6 +312,7 @@ fn arg(c : &RistrettoPoint, nums : &Vec<usize>, r : &Scalar, m : &Vec<Scalar>, g
     arg
 }
 
+/* Fonction qui permet au vérifieur de vérifier si la preuve rand est accepter */ 
 fn verirand(zkr : &Zkrand) -> u8 {
     let mut e = Scalar::hash_from_bytes::<Sha512>(zkr.arg.c_d.compress().as_bytes()) + Scalar::hash_from_bytes::<Sha512>(zkr.arg.c_delta.compress().as_bytes()) + Scalar::hash_from_bytes::<Sha512>(zkr.arg.c_a.compress().as_bytes());
     e = Scalar::hash_from_bytes::<Sha512>(e.as_bytes());
@@ -367,6 +376,7 @@ fn verirand(zkr : &Zkrand) -> u8 {
     return 1
 }
 
+// Fonction de calcul des theta avec aussi la preuve ZK sur les theta
 fn caltheta(theta : &mut Vec <Vec<RistrettoPoint>> , ch : &Vec<Chiffre> , sk : Scalar , n : usize, piij : &mut Vec<Zktheta>) {
     let mut i = 0;
     let mut j = 0;
@@ -382,6 +392,7 @@ fn caltheta(theta : &mut Vec <Vec<RistrettoPoint>> , ch : &Vec<Chiffre> , sk : S
     }
 }
 
+/* Fonction qui effectue la preuve des theta jusqu'au moment des calculs du vérifieur */
 fn zktheta(theta : &RistrettoPoint, sk : &Scalar, g : &RistrettoPoint) -> Zktheta {
     let mut rng = OsRng;
     let r : Scalar = Scalar::random(&mut rng);
@@ -392,6 +403,7 @@ fn zktheta(theta : &RistrettoPoint, sk : &Scalar, g : &RistrettoPoint) -> Zkthet
     pij
 }
 
+/* Fonction qui permet au vérifieur de vérifier si la preuve des theta est accepter */
 fn veritheta(zk : &Zktheta) -> u8 {
     let b = Scalar::hash_from_bytes::<Sha512>(zk.g.compress().as_bytes());
     if zk.z * zk.g == zk.y + b * zk.theta {
@@ -401,6 +413,7 @@ fn veritheta(zk : &Zktheta) -> u8 {
     }
 }
 
+/* Fonction qui fait en sorte que dans chaque chiffré il ne reste qu'une clé secrète correspondant au joueur à qui appartient la carte */
 fn calci(ch : &Vec<Chiffre> , theta : &Vec<Vec<RistrettoPoint>> , piij : &Vec<Zktheta>) -> Vec<Chiffre> {
     let mut ci : Vec <Chiffre> = Vec::new();
     let mut ind = vec![0usize;3];
@@ -441,6 +454,7 @@ fn calci(ch : &Vec<Chiffre> , theta : &Vec<Vec<RistrettoPoint>> , piij : &Vec<Zk
     ci
 }
 
+/* Fonction qui permet de déchiffrer les cartes une par une */
 fn dec(ch : &Vec<Chiffre>, sk : &Scalar, deck : &Vec<u32>, n : usize, g : &RistrettoPoint) -> u32 {
     let carte = ch[n].y - (sk * ch[n].x);
     let mut valeur = 0u32;
@@ -454,6 +468,7 @@ fn dec(ch : &Vec<Chiffre>, sk : &Scalar, deck : &Vec<u32>, n : usize, g : &Ristr
     valeur
 }
 
+// Fonction qui permet d'afficher les mains des quatre joueurs afin de vérifier le bon déroulement du jeu
 fn get_hand(deck : &Vec<u32>, ch : &Vec<Chiffre>, sk : &Scalar, n : usize, g : &RistrettoPoint) {
     let mut decksor : Vec<u32> = Vec::new();
     for i in 13*n..13*(n+1) {
@@ -464,9 +479,11 @@ fn get_hand(deck : &Vec<u32>, ch : &Vec<Chiffre>, sk : &Scalar, n : usize, g : &
     println!();
 }
 
+/* Fonction de jeu où à chaque exécution une carte est jouée */
 fn play(n : &usize, sk : &Scalar, pk : &Vec<RistrettoPoint>, chiffre : &Vec<Chiffre>, state : &State, state2 : &mut State, deck : &Vec<u32>, tour : &usize, g : &RistrettoPoint, jt : &mut Vec<usize>) -> (usize,u32,Zkpi0,Vec<Zkpij>,Vec<usize>) {
     let mut rng = thread_rng();
     let mut t = rng.gen_range(0..jt.len());
+    /* boucle while qui fait en sorte que la carte que le joueur veut jouer n'a pas été déjà jouée ou avec laquelle il a déjà tenté de jouer ce tour-ci */
     while (ine(&state.u[*n],&jt[t]) == 1){
         jt.remove(t);
         t = rng.gen_range(0..jt.len());
@@ -510,6 +527,7 @@ fn play(n : &usize, sk : &Scalar, pk : &Vec<RistrettoPoint>, chiffre : &Vec<Chif
     (t,valeur,pi0,pij,l)
 }
 
+/* Fonction qui vérifie si une carte n'a pas déjà été jouée. Si c'est le cas elle retourne 1 */
 fn ine(v : &Vec<usize>, j : &usize) -> u8{
     let k : usize = *j;
     if v.is_empty() {
@@ -523,6 +541,7 @@ fn ine(v : &Vec<usize>, j : &usize) -> u8{
     return 0
 }
 
+/* Fonction qui effectue la preuve pi0 jusqu'au moment des calculs du vérifieur */
 fn zkpi0(id : &u32, pk : &RistrettoPoint, g : &RistrettoPoint, ch : &Chiffre, sk : &Scalar) -> Zkpi0 {
     let mut rng = OsRng;
     let r : Scalar = Scalar::random(&mut rng);
@@ -537,6 +556,7 @@ fn zkpi0(id : &u32, pk : &RistrettoPoint, g : &RistrettoPoint, ch : &Chiffre, sk
     pi0
 }
 
+/* Fonction qui permet au vérifieur de vérifier si la preuve rand est accepter */
 fn veripi0(zk : Zkpi0) -> u8 {
     let pk2 = zk.ch.y - Scalar::from(zk.id) * zk.g;
     let mut c = Scalar::hash_from_bytes::<Sha512>(zk.t1.compress().as_bytes()) + Scalar::hash_from_bytes::<Sha512>(zk.t2.compress().as_bytes());
@@ -548,6 +568,7 @@ fn veripi0(zk : Zkpi0) -> u8 {
     }
 }
 
+/*  Fonction qui effectue la preuve pij jusqu'au moment des calculs du vérifieur */
 fn zkpij(deck : &Vec<u32>, l : &Vec<usize>, sk : &Scalar, pk : &RistrettoPoint, g : &RistrettoPoint, ch : &Chiffre, id : u32) -> Zkpij {
     let mut rng = OsRng;
     let mut indice = 39usize;
@@ -591,6 +612,7 @@ fn zkpij(deck : &Vec<u32>, l : &Vec<usize>, sk : &Scalar, pk : &RistrettoPoint, 
     pij
 }
 
+/* Fonction qui permet au vérifieur de vérifier si la preuve rand est accepter */ 
 fn veripij(pij : &Zkpij, deck : &Vec<u32>, l : &Vec<usize>) -> u8 {
     let mut s = Scalar::zero();
     for i in 0..39 {
@@ -615,7 +637,9 @@ fn veripij(pij : &Zkpij, deck : &Vec<u32>, l : &Vec<usize>) -> u8 {
     return 1
 }
 
+/* Fonction qui permet au vérifieur de vérifier si la carte que le joueur tente de jouer est jouable */ 
 fn verif(n : &usize, state : &State, state2 : &State, deck : &Vec<u32>, tour : &usize, t : &usize, carte : &u32, pi0 : Zkpi0, pij : &Vec<Zkpij>, l : &Vec<usize>, jt : &mut Vec<usize>) -> u8 {
+    /* Les quatre premières vérifications permettent de vérifier si il n'y a pas de problème dans state ou state2 */
     let mut i = 0;
     while i < 4 {
         if i == *n {
@@ -640,9 +664,11 @@ fn verif(n : &usize, state : &State, state2 : &State, deck : &Vec<u32>, tour : &
     if state.alpha != 4 && state.suit != 0 && (state2.alpha != state.alpha + 1||state2.suit != state.suit) {
         return 0;
     }
+    /* Vérifie si la carte est associée au bon joueur */
     if veripi0(pi0) == 0 {
         return 0
     }
+    /* Vérifie si le joueur a une carte de la couleur demandée si la carte qu'il veut jouer n'est pas de la bonne couleur */
     if (state2.suit != carte/100){
         for j in 0..13 {
             if pij[j].b == true {
@@ -656,6 +682,7 @@ fn verif(n : &usize, state : &State, state2 : &State, deck : &Vec<u32>, tour : &
     return 1;
 }
 
+/* Fonction qui permet de copier les éléments de state2 dans state */
 fn copy(state : &mut State, state2 : &State) {
     state.alpha = state2.alpha;
     state.suit = state2.suit;
@@ -676,12 +703,12 @@ fn main() {
         sk.push(r);
         pk.push(s);
     }
-    let mut pkc : RistrettoPoint = pk[0];
+    let mut pkc : RistrettoPoint = pk[0];// pkc est la somme des 4 clés publiques
     for i in 1..4{
         pkc = pkc + pk[i];
     }
-    let mut ch : Vec <Chiffre> = Vec::new();
-    let mut chiffre : Vec<Chiffre> = Vec::new();
+    let mut ch : Vec <Chiffre> = Vec::new();// ch correspnd au vecteur c dans le papier
+    let mut chiffre : Vec<Chiffre> = Vec::new();// chiffre correspnd au vecteur c dans le papier
     for i in 0..52 {
         let c : RistrettoPoint = pkc + g * Scalar::from(deck[i]);
         let d = Chiffre {x: g, y: c};
@@ -707,6 +734,7 @@ fn main() {
             while veri == 0 {
                 let (t,carte,pi0,pij,l) = play(&pre,&sk[pre],&pk,&chiffre,&state,&mut state2,&deck,&tour,&g,&mut jt);
                 veri = verif(&pre,&state,&state2,&deck,&tour,&t,&carte,pi0,&pij,&l,&mut jt);
+                /* Vérifications pour savoir au prochain tour quel va être le joueur à jouer en premier */
                 if n == 0 {
                     best = carte;
                     continue;
@@ -717,7 +745,9 @@ fn main() {
                 if carte/100 == best/100 && carte%100 > best%100 {
                     best = carte;
                     pl = pre;
-                    continue;                    }
+                    continue;
+                }
+                /* On suppose ici que la couleur Pique qui est la couleur dominante, est égale à 4 */
                 if carte/100 == 4 && carte/100 != best/100 {
                     best = carte;
                     pl = pre;
